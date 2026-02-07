db057dc39c0e57559eb6c096e1c4c5054a5ed955
Abhishekp121 <124229895+Abhishekp121@users.noreply.github.com>
2026-02-07 12:56:37 +0530

    singup

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import MainLayout from "./components/MainLayout";
 // Pages
 
 import Home from "./pages/Home";
+import singup from "./components/singup";
 
 
 export default function App() {
@@ -31,6 +32,7 @@ export default function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/singup" element={<singup />} />
          
         </Route>
       </Routes>
---
+++ src/components/singup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function singup() {
+  return (
+    <div>
+      <h1>singup</h1>
+    </div>
+  )
+}

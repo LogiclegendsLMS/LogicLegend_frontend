554b60beca0ba3ed2de946d1c9825d79a35b176c
aditya1375 <beleaditya56@gmail.com>
2026-03-07 18:37:21 +0530

    sub admin and teacher dashboard create

---
+++ src/subadmin/SubAdminCourses.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Courses from '../admin/Courses'
+
+function SubAdminCourses() {
+  return (
+    <div>
+      <Courses/>
+    </div>
+  )
+}
+
+export default SubAdminCourses
